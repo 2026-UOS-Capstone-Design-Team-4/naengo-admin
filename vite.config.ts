622d9b9e80c83219b5636df8083aaa5b3fe7c881
Narin Kim <ppoobb9447@gmail.com>
2026-06-02 22:53:14 +0900

    서비스 도메인 및 인증 API 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/auth-api': {
-        target: 'https://ai.naengo.com',
+        target: 'https://api.naengo.com',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/auth-api/, '/api/v1'),
       },
